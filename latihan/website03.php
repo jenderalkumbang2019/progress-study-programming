e32e7e237209cbb2beb82b07cb2bab595ab906e4
jenderalkumbang2019 <jenderalkumbang2019@gmail.com>
2020-06-08 21:20:01 +0700

    menambahkan folder latihan dan 2 file website yg dibuat

---
+++ latihan/website03.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>My Website</title>
+		<link rel="stylesheet" type="text/css" href="style03.css">
+	</head>
+	<body>
+		
+		<!-- topbar -->
+			<div class="topbar"></div>
+		<!-- end topbar -->
+			
+		<!-- CONTAINER -->
+		<div class="container">
+			
+			<!-- header -->
+			<div class="header">
+				
+				<a href="https://www.facebook.com/jerry.rinoldy" target="_blank">
+					<img 
+						src="img/facebookribbon1.png" 
+						alt="facebook ribbon"
+					/>
+				</a>
+				
+				<!--
+				<a href="https://www.facebook.com/jerry.rinoldy" class="facebook">
+					@jerryrinoldy
+				</a>
+				-->
+				<h1 class="judul">My Website</h1>
+				<ul>
+					<li><a href='#'>home</a></li>
+					<li><a href='#'>about</a></li>
+					<li><a href='#'>products</a></li>
+					<li><a href='#'>services</a></li>
+					<li><a href='#'>contact</a></li>
+				</ul>
+			</div>
+			<!-- end header -->
+			
+			<!-- hero -->
+			<div class="hero"></div>
+			<!-- end hero -->
+			
+			<!-- content -->
+			<div class="content">
+				
+				<!-- main -->
+				<div class="main">
+				
+					<h2>Judul Artikel</h2>
+					<p class="penulis">
+						ditulis oleh <a href="#">jerryrinoldy</a> pada 1 Januari 2020
+					</p>
+					<p>
+						malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel facilisis volutpat est velit egestas dui id ornare arcu odio ut sem nulla pharetra diam sit amet nisl suscipit adipiscing bibendum est ultricies integer quis auctor elit sed vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt nunc pulvinar
+					</p>
+					<p>
+						risus pretium quam vulputate dignissim suspendisse in est ante in nibh mauris cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit amet nulla facilisi morbi tempus iaculis urna id volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim convallis aenean et tortor at risus viverra adipiscing at in tellus integer feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit amet porttitor eget dolor morbi non arcu risus quis varius quam quisque id diam vel quam elementum pulvinar etiam non quam lacus suspendisse faucibus interdum posuere lorem ipsum dolor sit amet consectetur
+					</p>
+					<p>
+						blandit turpis cursus in hac habitasse platea dictumst quisque sagittis purus sit amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris a diam maecenas sed enim ut sem viverra aliquet eget sit amet tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt dui ut ornare lectus sit amet est placerat in egestas erat imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor leo
+					</p>
+				
+				</div>
+				<!-- end main -->
+				
+				<!-- side bar -->
+				<div class="sidebar">
+					<h3>Tentang Penulis</h3>
+					<img src="img/alex.png" alt="foto penulis">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Felis eget nunc lobortis mattis aliquam faucibus purus in. Tincidunt praesent semper feugiat nibh sed pulvinar proin.
+					</p>
+				</div>
+				<!-- end side bar -->
+				
+			</div>
+			<!-- end content -->
+			
+			<br>
+		
+			<!-- footer -->
+			<div class="footer">
+				<p class="copyright">Copyright 2020 Jerry Rinoldy</p>
+			</div>
+			<!-- end footer -->
+			
+		</div>
+	
+	</body>
+</html>
